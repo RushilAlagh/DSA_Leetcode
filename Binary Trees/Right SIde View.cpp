e5f7f58f57a67cafbf273b6532ca7a1e98976f8c
Rushil <164239717+RushilAlagh@users.noreply.github.com>
2025-09-08 01:31:22 +0530

    Create Right SIde View.cpp

---
+++ Binary Trees/Right SIde View.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    void dfs(TreeNode* node, int depth, vector<int>& result) {
+        if (node == nullptr) return;
+
+        if (depth == result.size()) {
+            result.push_back(node->val);
+        }
+
+        dfs(node->right, depth + 1, result);
+        dfs(node->left, depth + 1, result);
+    }
+
+    vector<int> rightSideView(TreeNode* root) {
+        vector<int> result;
+        dfs(root, 0, result);
+        return result;
+    }
+};
